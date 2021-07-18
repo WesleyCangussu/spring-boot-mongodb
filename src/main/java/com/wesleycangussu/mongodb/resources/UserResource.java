4adf78bc545d5c50817cd7cfb5c7e34c96aba935
Wesley Cangussu <wesley.cangussu@outlook.com>
2021-07-18 17:21:38 -0300

    EndPoint para retornar os posts de um usuario

--- src/main/java/com/wesleycangussu/mongodb/resources/UserResource.java
+++ src/main/java/com/wesleycangussu/mongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wesleycangussu.mongodb.domain.Post;
 import com.wesleycangussu.mongodb.domain.User;
 import com.wesleycangussu.mongodb.dto.UserDTO;
 import com.wesleycangussu.mongodb.services.UserService;
@@ -66,6 +67,13 @@ public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable St
 		
 		obj = uServ.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = uServ.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
