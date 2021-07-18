cb0f51625d9a5fd670b00989ef84b7f06e97586c
Wesley Cangussu <wesley.cangussu@outlook.com>
2021-07-18 16:59:18 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/wesleycangussu/mongodb/config/Instatiation.java
+++ src/main/java/com/wesleycangussu/mongodb/config/Instatiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.wesleycangussu.mongodb.domain.Post;
 import com.wesleycangussu.mongodb.domain.User;
+import com.wesleycangussu.mongodb.dto.AuthorDTO;
 import com.wesleycangussu.mongodb.repository.PostRepository;
 import com.wesleycangussu.mongodb.repository.UserRepository;
 
@@ -35,11 +36,13 @@ public class Instatiation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "BobGrey", "bob@gmail.com");
-
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Vou viajar", "Viajar para são paulo", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia","Acordei feliz hoje", maria);
 		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Vou viajar", "Viajar para são paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia","Acordei feliz hoje",new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 	
