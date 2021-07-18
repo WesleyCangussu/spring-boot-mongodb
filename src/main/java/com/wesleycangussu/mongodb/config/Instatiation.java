9df63a464cd770f4dea528d98562fc322aa147b8
Wesley Cangussu <wesley.cangussu@outlook.com>
2021-07-18 20:17:00 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/wesleycangussu/mongodb/config/Instatiation.java
+++ src/main/java/com/wesleycangussu/mongodb/config/Instatiation.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import com.wesleycangussu.mongodb.domain.Post;
 import com.wesleycangussu.mongodb.domain.User;
 import com.wesleycangussu.mongodb.dto.AuthorDTO;
+import com.wesleycangussu.mongodb.dto.CommentDTO;
 import com.wesleycangussu.mongodb.repository.PostRepository;
 import com.wesleycangussu.mongodb.repository.UserRepository;
 
@@ -42,6 +43,12 @@ public class Instatiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Vou viajar", "Viajar para são paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia","Acordei feliz hoje",new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa Viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
