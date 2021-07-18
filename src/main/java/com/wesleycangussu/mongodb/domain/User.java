22bd7023ca89ec9a72bb8e8885c6e66836651600
Wesley Cangussu <wesley.cangussu@outlook.com>
2021-07-18 17:12:43 -0300

    Referenciando os posts dos usuarios

--- src/main/java/com/wesleycangussu/mongodb/domain/User.java
+++ src/main/java/com/wesleycangussu/mongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.wesleycangussu.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -15,6 +18,8 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -49,11 +54,17 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
+	
 
 	@Override
 	public boolean equals(Object obj) {
