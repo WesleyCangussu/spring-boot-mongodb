7ef439ba88a161d52b73be889e2a0742e8e18997
Wesley Cangussu <wesley.cangussu@outlook.com>
2021-07-18 23:56:17 -0300

    Consulta com varios criterios

--- src/main/java/com/wesleycangussu/mongodb/resources/PostResource.java
+++ src/main/java/com/wesleycangussu/mongodb/resources/PostResource.java
@@ -1,8 +1,7 @@
 package com.wesleycangussu.mongodb.resources;
 
+import java.util.Date;
 import java.util.List;
-
-import javax.websocket.Decoder.Text;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -38,5 +37,17 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value = "/searchall")
+	public ResponseEntity<List<Post>> searchAll(
+		@RequestParam(value = "text",defaultValue = "") String text ,
+		@RequestParam(value = "minDate",defaultValue = "") String minDate ,
+		@RequestParam(value = "maxDate",defaultValue = "") String maxDate ) {
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date (0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = pServ.searchAll(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 	
 }
